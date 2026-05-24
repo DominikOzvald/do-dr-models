2a76d3607b8eccb41e8199abcaeec50141f1409c
DominikOzvald <dominik.ozvald@gmail.com>
2026-05-24 11:17:45 +0200

    Update fetch_logs.py

--- fetch_logs.py
+++ fetch_logs.py
@@ -10,6 +10,7 @@ def fetch_log(token: str, job_name: str, repo: str, run_id: str):
         "X-GitHub-Api-Version": "2022-11-28",
     }
     url = f"https://api.github.com/repos/{repo}/actions/runs/{run_id}/jobs"
+    print(f"Looking for job {job_name}")
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
@@ -18,11 +19,23 @@ def fetch_log(token: str, job_name: str, repo: str, run_id: str):
         for job in jobs:
             if job["name"] == job_name:
                 job_id = job["id"]
-            print(f"job_id {job_id}")
+                print(f"Found job {job_name}")
         if job_id:
             url = f"https://api.github.com/repos/{repo}/actions/jobs/{job_id}/logs"
             response = requests.get(url, headers=headers)
-            print(f"response status {response.status_code}")
+            if response.status_code == 200:
+                with open(f"{job_name}.txt", "w") as f:
+                    f.write(response.text)
+                print(f"Successfully loaded logs to file  {job_name}.txt")
+            else:
+                print(
+                    f"Failed to load logs. Response status code {response.status_code}"
+                )
+                exit(-1)
+        else:
+            print(f"Could not find job {job_name}")
+            exit(-1)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
